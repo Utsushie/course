a5afd74135ef995a3a254e759bcfc6209ac5474f
yanxin <178556008@qq.com>
2021-08-30 18:32:53 +0800

    处理login页面

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -20,90 +20,74 @@
 			
 					<div class="space-6"></div>
 
-						<div class="position-relative">
-							<div id="login-box" class="login-box visible widget-box no-border">
-								<div class="widget-body" style="background-color:rgba(0,0,0,0)">
-									<div class="widget-main" style="background-color:rgba(0,0,0,0)">
-										<!-- <h4 class="header blue lighter bigger">
-											<i class="ace-icon fa fa-coffee green"></i>
-											Please Enter Your Information
-										</h4> -->
-								
-										<div class="space-6"></div>
+					<div class="position-relative">
+						<div id="login-box" class="login-box visible widget-box no-border">
+							<div class="widget-body" style="background-color:rgba(0,0,0,0)">
+								<div class="widget-main" style="background-color:rgba(0,0,0,0)">
+									<!-- <h4 class="header blue lighter bigger">
+										<i class="ace-icon fa fa-coffee green"></i>
+										Please Enter Your Information
+									</h4> -->
+							
+									<div class="space-6"></div>
 
-											<form>
-												<fieldset>
-													<label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input type="text" class="form-control" placeholder="Username" />
-															<i class="ace-icon fa fa-user"></i>
-														</span>
+										<form>
+											<fieldset>
+												<label class="block clearfix">
+													<span class="block input-icon input-icon-right">
+														<input type="text" class="form-control" placeholder="Username" />
+														<i class="ace-icon fa fa-user"></i>
+													</span>
+												</label>
+										
+												<label class="block clearfix">
+													<span class="block input-icon input-icon-right">
+														<input type="password" class="form-control" placeholder="Password" />
+														<i class="ace-icon fa fa-lock"></i>
+													</span>
+												</label>
+
+												<div class="space"></div>
+
+												<div class="clearfix">
+													<label class="inline">
+														<input type="checkbox" class="ace" />
+														<span class="lbl" style="color:#D67E31">记住我</span>
 													</label>
-											
-													<label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input type="password" class="form-control" placeholder="Password" />
-															<i class="ace-icon fa fa-lock"></i>
-														</span>
-													</label>
 
-													<div class="space"></div>
+													<button type="button" class="width-35 pull-right btn btn-sm btn-primary">
+														<i class="ace-icon fa fa-key"></i>
+														<span class="bigger-110">登录</span>
+													</button>
+												</div>
 
-													<div class="clearfix">
-														<label class="inline">
-															<input type="checkbox" class="ace" />
-															<span class="lbl" style="color:#D67E31">记住我</span>
-														</label>
+												<div class="space-4"></div>
+											</fieldset>
+										</form>
+							
+									<div class="social-or-login center">
+										<span class="bigger-110">使用第三方登录</span>
+									</div>
 
-														<button type="button" class="width-35 pull-right btn btn-sm btn-primary">
-															<i class="ace-icon fa fa-key"></i>
-															<span class="bigger-110">登录</span>
-														</button>
-													</div>
+									<div class="space-6"></div>
 
-													<div class="space-4"></div>
-												</fieldset>
-											</form>
-								
-										<div class="social-or-login center">
-											<span class="bigger-110">使用第三方登录</span>
-										</div>
+									<div class="social-login center">
+										<a class="btn btn-primary">
+											<i class="ace-icon fa fa-wechat"></i>
+										</a>
 
-										<div class="space-6"></div>
+										<a class="btn btn-info">
+											<i class="ace-icon fa fa-qq"></i>
+										</a>
 
-										<div class="social-login center">
-											<a class="btn btn-primary">
-												<i class="ace-icon fa fa-wechat"></i>
-											</a>
-
-											<a class="btn btn-info">
-												<i class="ace-icon fa fa-qq"></i>
-											</a>
-
-											<a class="btn btn-danger">
-												<i class="ace-icon fa fa-weibo"></i>
-											</a>
-										</div>
-									</div><!-- /.widget-main -->
-								</div><!-- /.widget-body -->
-							</div><!-- /.login-box -->
-
-							</div><!-- /.position-relative -->
-				
-					<div class="navbar-fixed-top align-right">
-						<br />
-						&nbsp;
-						<a id="btn-login-dark" href="#">Dark</a>
-						&nbsp;
-						<span class="blue">/</span>
-						&nbsp;
-						<a id="btn-login-blur" href="#">Blur</a>
-						&nbsp;
-						<span class="blue">/</span>
-						&nbsp;
-						<a id="btn-login-light" href="#">Light</a>
-						&nbsp; &nbsp; &nbsp;
-					</div>
+										<a class="btn btn-danger">
+											<i class="ace-icon fa fa-weibo"></i>
+										</a>
+									</div>
+								</div><!-- /.widget-main -->
+							</div><!-- /.widget-body -->
+						</div><!-- /.login-box -->
+					</div><!-- /.position-relative -->
 				</div>
 			</div><!-- /.col -->
 		</div><!-- /.row -->
