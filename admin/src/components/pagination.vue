77eecac3ef4b359139450f3efb3aa2cab705b20a
Hirasawa Yui <178556008@qq.com>
2021-09-02 05:57:48 +0800

    前端分页组件(猫咪睡屏幕前了,下次再写)

---
+++ admin/src/components/pagination.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="pagination" role="group" aria-label="分页">
+		<button type="button" class="btn btn-default btn-white btn-round"
+			v-bind:disabled="page === 1"
+			v-on:click="selectPage(1)">
+			1
+		</button>
+		<button type="button" class="btn btn-default btn-white btn-round"
+			v-bind:disabled="page === 1"
+			v-on:click="selectPage(page - 1)">
+			上一页
+		</button>
+		<button v-for="p in pages" v-bind:id="'page-' + p"
+			type="button" class="btn btn-default btn-white btn-round"
+			v-bind:class="{'btn-primary active':page == p}"
+			v-on:click="selectPage(p)">
+			{{p}}
+		</button>
+		<button type="button" class="btn btn-default btn-white btn-round"
+			v-bind:disabled="page === pageTotal"
+			v-on:click="selectPage(page + 1)">
+			下一页
+		</button>
+		<button type="button" class="btn btn-default btn-white btn-round"
+			v-bind:disabled="page === pageTotal"
+			v-on:click="selectPage(1)">
+			1
+		</button>
+	</div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				page: 1,
+				pages:[],
+				pageTotal:''
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,81 +1,94 @@
 <template>
-	<table id="simple-table" class="table  table-bordered table-hover">
-		<thead>
-			<tr>
-				<th>ID</th>
-				<th>课程ID</th>
-				<th>名称</th>
-				<th>操作</th>
-			</tr>
-		</thead>
-
-		<tbody>
-			<tr v-for="chapter in chapterList">
-				<td>{{chapter.id}}</td>
-				<td>{{chapter.courseId}}</td>
-				<td>{{chapter.name}}</td>
-
-				<td>
-					<div class="hidden-sm hidden-xs btn-group">
-						<button class="btn btn-xs btn-success">
-							<i class="ace-icon fa fa-check bigger-120"></i>
-						</button>
-
-						<button class="btn btn-xs btn-info">
-							<i class="ace-icon fa fa-pencil bigger-120"></i>
-						</button>
-
-						<button class="btn btn-xs btn-danger">
-							<i class="ace-icon fa fa-trash-o bigger-120"></i>
-						</button>
-
-						<button class="btn btn-xs btn-warning">
-							<i class="ace-icon fa fa-flag bigger-120"></i>
-						</button>
-					</div>
-
-					<div class="hidden-md hidden-lg">
-						<div class="inline pos-rel">
-							<button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
-								<i class="ace-icon fa fa-cog icon-only bigger-110"></i>
+	<div>
+		<p>
+			<button class="btn btn-primary" @click="getChapterList(page)">
+				<i class="ace-icon fa fa-search"></i>
+				查询
+			</button>
+		</p>
+		<table id="simple-table" class="table  table-bordered table-hover">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>课程ID</th>
+					<th>名称</th>
+					<th>操作</th>
+				</tr>
+			</thead>
+		
+			<tbody>
+				<tr v-for="chapter in chapterList">
+					<td>{{chapter.id}}</td>
+					<td>{{chapter.courseId}}</td>
+					<td>{{chapter.name}}</td>
+		
+					<td>
+						<div class="hidden-sm hidden-xs btn-group">
+							<button class="btn btn-xs btn-success">
+								<i class="ace-icon fa fa-check bigger-120"></i>
 							</button>
-
-							<ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-								<li>
-									<a href="#" class="tooltip-info" data-rel="tooltip" title="View">
-										<span class="blue">
-											<i class="ace-icon fa fa-search-plus bigger-120"></i>
-										</span>
-									</a>
-								</li>
-
-								<li>
-									<a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
-										<span class="green">
-											<i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
-										</span>
-									</a>
-								</li>
-
-								<li>
-									<a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
-										<span class="red">
-											<i class="ace-icon fa fa-trash-o bigger-120"></i>
-										</span>
-									</a>
-								</li>
-							</ul>
+		
+							<button class="btn btn-xs btn-info">
+								<i class="ace-icon fa fa-pencil bigger-120"></i>
+							</button>
+		
+							<button class="btn btn-xs btn-danger">
+								<i class="ace-icon fa fa-trash-o bigger-120"></i>
+							</button>
+		
+							<button class="btn btn-xs btn-warning">
+								<i class="ace-icon fa fa-flag bigger-120"></i>
+							</button>
 						</div>
-					</div>
-				</td>
-			</tr>
-		</tbody>
-	</table>
+		
+						<div class="hidden-md hidden-lg">
+							<div class="inline pos-rel">
+								<button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
+									<i class="ace-icon fa fa-cog icon-only bigger-110"></i>
+								</button>
+		
+								<ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
+									<li>
+										<a href="#" class="tooltip-info" data-rel="tooltip" title="View">
+											<span class="blue">
+												<i class="ace-icon fa fa-search-plus bigger-120"></i>
+											</span>
+										</a>
+									</li>
+		
+									<li>
+										<a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
+											<span class="green">
+												<i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
+											</span>
+										</a>
+									</li>
+		
+									<li>
+										<a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
+											<span class="red">
+												<i class="ace-icon fa fa-trash-o bigger-120"></i>
+											</span>
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		<pagination ref="pagination" v-bind:list="getChapterList"></pagination>
+	</div>
 	
 </template>
 
 <script>
+	
+	import Pagination from "../../components/pagination.vue"
+	
 	export default {
+		components: {Pagination},
 		name: 'chapter',
 		data(){
 			return{
@@ -83,19 +96,20 @@
 			}
 		},
 		mounted:function(){
-			this.getChapterList();
+			let _this = this;
+			_this.getChapterList(1);
 		},
 		methods:{
-			getChapterList(){
+			getChapterList(page){
 				let _this = this;
-				_this.$ajax.get('http://127.0.0.1:9000/business/admin/chapter/getChapterList',{
-					params:{
-						page:1,
-						size:10
+				let params = {
+						page:page,
+						size:_this.$refs.pagination.size
 					}
-				}).then((response)=>{
+				_this.$ajax.get('http://127.0.0.1:9000/business/admin/chapter/getChapterList',{params}).then((response)=>{
 					console.log("列表接口返回结果",response);
-					this.chapterList = response.data.list;
+					_this.chapterList = response.data.list;
+					_this.$refs.pagination.render(page,response,data.total);
 				})
 			}
 		}
