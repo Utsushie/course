20a86918cea71114c81a8fb602581ebae17685c1
yanxin <178556008@qq.com>
2021-09-15 18:30:57 +0800

    自动生成dto模板 1

--- generator/src/main/java/com/course/generator/server/GeneratorServer.java
+++ generator/src/main/java/com/course/generator/server/GeneratorServer.java
@@ -1,11 +1,12 @@
 package com.course.generator.server;
 
+import com.course.generator.util.DbUtil;
+import com.course.generator.util.Field;
 import com.course.generator.util.FreeMarkerUtil;
 import freemarker.template.TemplateException;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @Author YanXin
@@ -14,23 +15,47 @@ import java.util.Map;
 public class GeneratorServer {
 
 	static String MODULE = "business";
-	static String toServerPath = "business\\src\\main\\java\\com\\course\\business\\controller\\admin\\";
-	//static String toServerPath = "server\\src\\main\\java\\com\\course\\server\\service\\";
-	//static String toServerPath = "server\\src\\main\\java\\com\\course\\server\\service\\impl\\";
+	static String toDtoPath = "server\\src\\main\\java\\com\\course\\server\\dto\\";
+	static String toServerPath = "server\\src\\main\\java\\com\\course\\server\\service\\";
+	static String toServerImplPath = "server\\src\\main\\java\\com\\course\\server\\service\\impl\\";
 	static String toControllerPath = "business\\src\\main\\java\\com\\course\\" + MODULE +"\\controller\\admin\\";
 
-	public static void main(String[] args) throws IOException, TemplateException {
+	public static void main(String[] args) throws Exception {
 		String Domain = "Section";
 		String domain = "section";
 		String tableName = "小节";
 		String module = "business";
+		List<Field> fieldList = DbUtil.getColumnByTableName("cs_section");
+		Set<String> typeSet = getJavaTypes(fieldList);
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("Domain",Domain);
 		map.put("domain",domain);
 		map.put("tableName",tableName);
 		map.put("module",module);
+		map.put("fieldList", fieldList);
+		map.put("typeSet", typeSet);
+
+		//生成dto
+		FreeMarkerUtil.initConfig("dto.ftl");
+		FreeMarkerUtil.generator(toDtoPath + Domain + "Dto",map);
+
+		FreeMarkerUtil.initConfig("serviceImpl.ftl");
+		FreeMarkerUtil.generator(toServerImplPath + Domain + "ServiceImpl.java",map);
+
+		FreeMarkerUtil.initConfig("service.ftl");
+		FreeMarkerUtil.generator(toServerPath + Domain + "Service.java",map);
+
 		FreeMarkerUtil.initConfig("controller.ftl");
 		FreeMarkerUtil.generator(toControllerPath + Domain + "Controller.java",map);
+	}
+
+	private static Set<String> getJavaTypes(List<Field> fieldList){
+		Set<String> set = new HashSet<>();
+		for(int i = 0; i<fieldList.size();i++){
+			Field field = fieldList.get(i);
+			set.add(field.getJavaType());
+		}
+		return set;
 	}
 
 }
